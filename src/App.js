902f378e7dab5a7ba7aec21b7bdac3457e36509e
Tatyana Tupalo <tatyana080489t@gmail.com>
2021-07-20 07:10:21 +0300

    сборка

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import HomePage from 'components/HomePage/HomePage';
+import MovieDetailsPage from 'components/MovieDetailsPage/MovieDetailsPage';
+import MoviePage from 'components/MoviesPage/MoviesPage';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+      <MovieDetailsPage />
+      <MoviePage />
+    </div>
+  );
+}
+
+export default App;
