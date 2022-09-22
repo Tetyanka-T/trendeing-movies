3fc03e291424bf976338eeabfb2b5fd93b34f6b7
Tatyana Tupalo <tatyana080489t@gmail.com>
2022-09-22 15:26:38 +0300

    fix goback

--- src/components/GoBack/GoBack.js
+++ src/components/GoBack/GoBack.js
@@ -1,21 +1,8 @@
-import { useRef, useEffect } from 'react';
-import { useHistory, useLocation } from 'react-router-dom';
 import s from './GoBack.module.css';
 
 const ComeBackButton = () => {
-  const history = useHistory();
-  const location = useLocation();
-  const routerState = useRef(null);
-
-  useEffect(() => {
-    if (!routerState.current) {
-      routerState.current = location.state;
-    }
-  }, [location.state]);
-
   const goBack = () => {
-    const url = routerState.current ? `?${routerState.current.params}` : '/';
-    history.push(url);
+    window.history.back();
   };
 
   return (
